d80122b9459855ad894a971d2797d260abd7f72e
Jake Dluhy <jake.dluhy@producepay.com>
2019-08-27 17:08:01 -0700

    Switch to skipper-s3

--- config/files.js
+++ config/files.js
@@ -3,9 +3,5 @@
  * Options which relate to filesystem storage of assets
  */
 module.exports.files = {
-  // Maximum allowed file size in bytes
-  // Defaults to 500MB
-  maxBytes: 524288000,
-  // The fs directory name at which files will be kept
-  dirname: ''
+  adapter: require('skipper-s3'),
 };
